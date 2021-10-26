618eda5a5fd801fa7ce7f45020b00363c8935c83
Mark Barbone <mark.l.barbone@gmail.com>
2021-10-26 14:02:32 -0700

    Parser

--- src/Src.hs
+++ src/Src.hs
@@ -8,14 +8,24 @@ module Src where
 
 import Builtins
 
+import Control.Applicative
+import Control.Monad
 import Data.Text (Text)
 import Data.Text qualified as T
+import Data.Functor
+import Text.Parsec (Parsec, parse, eof, try, optionMaybe, (<?>), oneOf)
+import Text.Parsec.Char (letter, alphaNum, char, string)
+import Text.Parsec.Token
+import Text.Parsec.Expr
+import Data.Maybe
+
 
 type Name = Text
 
 data Ty = TVar Name
         | TUnit
         | TInt
+        | THole -- _ in a type
         | TPair Ty Ty
         | TFun Ty Ty
         | TForall Name Ty
@@ -31,6 +41,17 @@ data Exp = EVar Name
          -- | ELet [(Name, Maybe Ty, Exp)] Exp
          -- | EIf Exp Exp Exp
         deriving (Eq, Show)
+
+isSyntacticValue :: Exp -> Bool
+isSyntacticValue (ELam _ _ _) = True
+isSyntacticValue (EAnnot e _) = isSyntacticValue e
+isSyntacticValue _ = False
+
+-- | Top-level declarations/definitions
+-- 
+-- TODO: more things (standalone type declarations? imports? modules?)
+data Top = Defn Name (Maybe Ty) Exp
+         deriving Show
 
 epair :: Exp -> Exp -> Exp
 epair a b = EApp (EApp (EBuiltin Pair) a) b
@@ -59,4 +80,104 @@ badExample = ELet "f" (Just ty) (lam "x" \x -> EApp x (ELit 3))
 -- From here on, it's just parsing/pretty-printing
 
 -- TODO: parsing, pretty-printing
+
+parser :: Parsec String () [Top]
+exprParser :: Parsec String () Exp
+(parser, exprParser) = (whiteSpace *> many top, whiteSpace *> expr)
+  where
+    TokenParser{..} = makeTokenParser LanguageDef{
+        commentStart = "(*"
+      , commentEnd = "*)"
+      , commentLine = "--"
+      , nestedComments = True
+      , identStart = letter <|> char '_'
+      , identLetter = alphaNum <|> char '_'
+      , opStart = oneOf "!#$%&*+./<=>?@\\^|-~:"
+      , opLetter = oneOf "!#$%&*+./<=>?@\\^|-~:"
+      , reservedNames =
+        ["forall", "_", "if", "then", "else", "val", "let", "in", "end", "fun", "fn", "int"] -- TODO int is not reserved
+      , reservedOpNames = ["->", "=>", "=", ":", "*", "+", "-", "."]
+      , caseSensitive = True
+      }
+
+    ident = T.pack <$> identifier
+
+    letExpr = do
+      reserved "let"
+      ds <- many defn
+      reserved "in"
+      exp <- expr
+      reserved "end"
+      pure $ foldr (\(n,ty,val) -> ELet n ty val) exp ds
+
+    atomic = EVar <$> ident
+      <|> ELit . fromInteger <$> natural
+      <|> try (parens (pure $ EBuiltin Unit))
+      <|> parens expr
+      <|> letExpr
+      <?> "simple expression"
+
+    factor = foldl1 EApp <$> some atomic
+
+    arithExpr = buildExpressionParser table factor
+      <?> "arithmetic expression"
+    table = [[op "*" Mul], [op "+" Add, op "-" Sub]]
+    op name b =
+      Infix (reservedOp name $> \x y -> EApp (EApp (EBuiltin b) x) y) AssocLeft
+
+    annotExpr = do
+      e <- arithExpr
+      maybe e (EAnnot e) <$> optionMaybe (reservedOp ":" *> typ)
+    ifExpr = do
+      reserved "if"
+      _cond <- expr
+      reserved "then"
+      _trueBranch <- expr
+      reserved "else"
+      _falseBranch <- expr
+      pure $ error "TODO: implement if expressions"
+    lambda = do
+      reserved "fn"
+      args <- some param
+      reservedOp "=>"
+      body <- expr
+      pure $ foldr (uncurry ELam) body args
+    expr = ifExpr <|> lambda <|> annotExpr <?> "expression"
+
+    param :: Parsec String () (Name, Maybe Ty)
+    param = (, Nothing) <$> ident
+      <|> parens ((,) <$> ident <* reservedOp ":" <*> (Just <$> typ))
+      <?> "parameter"
+
+    simpleTyp = try (parens (pure TUnit))
+      <|> reserved "int" $> TInt
+      <|> reserved "_" $> THole
+      <|> TVar <$> ident
+      <|> parens typ
+    typ :: Parsec String () Ty
+    typ = liftA2 maybe id TFun <$> simpleTyp <*> optionMaybe (reservedOp "->" *> typ)
+      <|> flip (foldr TForall) <$> (reserved "forall" *> many ident) <*> (reservedOp "." *> typ)
+      <?> "type"
+
+    -- currently no difference, but eventually 'fun' will be a letrec
+    defn = val <|> fun <?> "definition"
+    val = do
+      reserved "val"
+      n <- ident
+      t <- optionMaybe (reservedOp ":" *> typ)
+      reservedOp "="
+      v <- expr
+      pure (n,t,v)
+    fun = do
+      reserved "fun"
+      n <- ident
+      args <- some param
+      retTy <- optionMaybe (reservedOp ":" *> typ)
+      reservedOp "="
+      body <- expr
+      let ty = foldr (\(_, argTy) -> TFun (fromMaybe THole argTy)) (fromMaybe THole retTy) args
+      pure (n, Just ty, foldr (uncurry ELam) body args)
+
+    top = (\(n,ty,val) -> Defn n ty val) <$> defn
+
 
